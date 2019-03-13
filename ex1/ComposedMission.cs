00daed09f69fb3563bbd9acf5411c0480917af9b
ניסן אגבייב <bknisan@gmail.com>
2019-03-13 02:55:25 -0700

    finished all. fixed null ptr problem in single mission. made container use dictionry.

---
+++ ex1/ComposedMission.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+    public class ComposedMission : IMission
+    {
+        private string myname;
+        private string mytype;
+        public string Name
+        {
+            get {return myname; }
+        }
+
+        public string Type
+        {
+            get { return mytype; }
+        }
+        public ComposedMission(string name)
+        {
+            myname = name;
+        }
+        public event EventHandler<double> OnCalculate;
+
+        public double Calculate(double value)
+        {
+            return 1;
+        }
+    }
+}
---
+++ ex1/SingleMission.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+    public class SingleMission : IMission
+    {
+        private string myname;
+        private string mytype;
+        public SingleMission(string name)
+        {
+            myname = name;
+        }
+
+        public string Name
+        {
+            get { return myname; }
+        }
+
+        public string Type
+        {
+            get {return mytype; }
+        }
+
+        public event EventHandler<double> OnCalculate;
+
+
+        public double Calculate(double value)
+        {
+        }
+    }
+}
